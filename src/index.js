886126f36a4a6e656293cfaf8d453d7811c4d934
Satish <satish_95raj@outlook.com>
2023-01-03 14:23:01 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const WordBox =()=>{
+  return <div className='wordBox'><input type="text" /></div>
+}
+
+const SearchButton =()=>{
+  return <div className='btn'><button className='btn1' type="submit">Google Search</button> <button className='btn2' type="submit">I'm Feeling Lucky</button></div>
+}
+
+const LanguageNote =(props)=>{
+  return <h4 className='lang'>Google offered in: <span>{props.title}</span></h4>
+}
 root.render(
   <React.StrictMode>
     <App />
+    <WordBox />
+    <SearchButton />
+    <LanguageNote title="English  हिन्दी"/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
